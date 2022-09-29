13d05c30fd3c12c5262d66c2eaa799d2d624c28e
Bair <73276298+zoxione@users.noreply.github.com>
2022-09-29 18:21:36 +0300

    0.32203 competition

--- lab1/main.py
+++ lab1/main.py
@@ -2,17 +2,15 @@
 # Отхонов Баир КТбо3-8
 # Бакухин Александр КТбо3-8
 
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.metrics import mean_absolute_error
-
-
 
 # Класс для хранения свойств
 class Property:
@@ -25,6 +23,7 @@ class Property:
 
 # Инициализация
 propertyList = []
+K_VALUE = 0.65
 
 
 def rmsle(y_true, y_pred):
@@ -59,48 +58,72 @@ def read_file():
             propertyList[len(propertyList) - 1].values.append(word)
 
 
+def df_string_to_number(df):
+   dfCopy = df.copy(deep=True)
+
+   for column in dfCopy.columns.values.tolist():
+      for property in propertyList:
+         if column == property.name:
+            for i in range(len(property.values)):
+               dfCopy[column] = dfCopy[column].replace([property.values[i]], int(len(property.values) - i))
+            break
+
+      # Зануляем пустые значения
+      dfCopy[column] = dfCopy[column].replace(np.nan, 0)
+
+      # Зануляем строки, которых нет в параметрах
+      for value in dfCopy[column]:
+         if type(value) is str:
+            dfCopy[column] = dfCopy[column].replace([value], 0)
+
+   return dfCopy
+
+
 # Start
 if __name__ == '__main__':
    # Чтение файла
    read_file()
-   print(propertyList[0].name)
 
    # Считываем данные как dataframe
    dfTrain = pd.read_csv('./train.csv')
-   print(dfTrain.head())
+   dfTrainCopy = df_string_to_number(dfTrain)
+   dfTrainCopy.to_csv('result_1.csv', index=False)
 
-   # Получаем столбцы
-   dfTabs = dfTrain.columns.values.tolist()
+   # Получаем коэффициенты корреляции (старое)
+   # for tab in dfTrain.columns.values.tolist():
+   #    if tab != 'SalePrice' and tab != 'Id':
+   #       # k = (pd.to_numeric(dfTrain[tab], errors='coerce', downcast='integer')).corr(dfTrain['SalePrice'])
+   #       # k = (pd.to_numeric(dfTrainCopy[tab], errors='coerce', downcast='integer')).corr(dfTrainCopy['SalePrice'])
+   #       k = dfTrainCopy[tab].corr(dfTrainCopy['SalePrice'])
+   #
+   #       for property in propertyList:
+   #          if property.name == tab:
+   #             property.k = k
+   #             break
 
    # Получаем коэффициенты корреляции
-   for tab in dfTabs:
-      if tab != 'SalePrice' and tab != 'Id':
-         k = (pd.to_numeric(dfTrain[tab], errors='coerce', downcast='integer')).corr(dfTrain['SalePrice'])
-
-         for property in propertyList:
-            if property.name == tab:
-               property.k = k
-               break
-
-   # cnt = 0
-   # for property in propertyList:
-   #    if pd.isna(property.k) == False and property.k > 0.6:
-   #       cnt += 1
-   #       sns.set_style("darkgrid")
-   #       plt.subplot(2, 3, cnt)
-   #       sns.lineplot(data=df, x=property.name, y="SalePrice")
-
-   # Отрисофка графиков
-   sns.set_style("darkgrid")
-   sns.pairplot(dfTrain[["SalePrice", "OverallQual", "TotalBsmtSF", "GrLivArea", "GarageCars", "GarageArea"]], height=2.5)
-
-
+   chooseProperty = []
    for property in propertyList:
+      property.k = dfTrainCopy[property.name].corr(dfTrainCopy['SalePrice'])
       print(property.name, property.k)
 
-   #X = dfTrain[['LotArea', 'OverallQual', 'YearBuilt', 'TotRmsAbvGrd']]
-   X = dfTrain[["SalePrice", "OverallQual", "TotalBsmtSF", "GrLivArea", "GarageCars", "GarageArea"]]
-   Y = dfTrain.SalePrice
+      if property.k >= K_VALUE:
+         chooseProperty.append(property.name)
+
+   # Отрисовываем графики
+   cnt = 0
+   for property in propertyList:
+      if property.k >= K_VALUE:
+         cnt += 1
+         sns.set_style("darkgrid")
+         plt.subplot(int(len(chooseProperty) / 2), 2, cnt)
+         sns.lineplot(data=dfTrainCopy, x=property.name, y="SalePrice")
+
+   print("\n\n ========================= \n")
+   print("[LOG] Count items in chooseProperty[]: " + str(cnt))
+
+   X = dfTrainCopy[chooseProperty]
+   Y = dfTrainCopy.SalePrice
 
    # Разделение на train и validation
    # Обучать будем на 75% данных, проверять на 25%
@@ -114,17 +137,33 @@ if __name__ == '__main__':
    lr = LinearRegression()
    lr.fit(X_train, y_train)
    LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
-
    prediction = lr.predict(X_test)
 
    mae = mean_absolute_error(y_test, prediction)
-   print(mae)
-   # 57324.261901630831
+   print("[LOG] mae: " + str(mae))
 
-   # Средняя абсолютная ошибка - 57к долларов для этой модели
+   # Средняя абсолютная ошибка
    result = rmsle(y_test, prediction)
-   print(result)
-   # 0.40943121669338522
+   print("[LOG] rmsle: " + str(result))
 
 
+   # Модель сделана, теперь нужно сделать предсказание на тестовых данных
+   dfTest = pd.read_csv('./test.csv')
+
+   dfTestCopy = df_string_to_number(dfTest)
+   dfTestCopy = dfTestCopy[chooseProperty]
+
+   dfTestCopy.to_csv('result_2.csv', index=False)
+
+   pred = lr.predict(dfTestCopy)
+   dfTestCopy.drop(chooseProperty, axis=1, inplace=True)
+   dfTestCopy['Id'] = dfTest['Id']
+   dfTestCopy['SalePrice'] = pred
+
+   dfTestCopy.to_csv('result_3.csv', index=False)
+
+   print("[LOG] Done!")
+
+   # Показать графики
    plt.show()
+
